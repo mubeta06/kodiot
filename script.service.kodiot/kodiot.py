bd2bad7701fb8241c109f0f1894e37a671ba6c6d
Matthew Baker <mu.beta.06@gmail.com>
2018-05-21 14:28:25 +1000

    cleaned up code a little added configurable thing name. handled restart on setting change.

--- script.service.kodiot/kodiot.py
+++ script.service.kodiot/kodiot.py
@@ -39,10 +39,71 @@ class Kodiot(xbmc.Monitor):
 
     def __init__(self):
         self.addon = xbmcaddon.Addon()
+        self.mqtt = client.Client()
+
+    def start(self):
+        """Starting Kodiot."""
+        LOG.info('Launching Kodiot %s, MQTT version %s', self.version,
+                 mqtt.__version__)
+        self.mqtt.reinitialise()
+        self.mqtt.on_connect = self.on_connect
+        self.mqtt.on_disconnect = self.on_disconnect
+        self.mqtt.on_subscribe = self.on_subscribe
+        self.mqtt.on_message = self.on_message
+        self.mqtt.tls_set(self.root_ca, certfile=self.pem, keyfile=self.key,
+                          cert_reqs=ssl.CERT_REQUIRED,
+                          tls_version=ssl.PROTOCOL_TLSv1_2)
+
+        # Connect with MQTT Broker
+        self.mqtt.connect(self.host, self.port, self.keepalive)
+        self.mqtt.loop_start()
+
+    def stop(self):
+        """Stop Kodiot."""
+        LOG.debug('Stopping Kodiot')
+        self.mqtt.loop_stop()
 
     def onSettingsChanged(self):
         """Called when addon settings are changed."""
-        LOG.debug('Kodiot: Settings changed')
+        LOG.debug('Kodiot Settings changed')
+        self.stop()
+        self.start()
+
+    def on_connect(self, mqttc, userdata, flags, rc):
+        """MQTT on connect callback function implementation."""
+        LOG.info('on_connect: %s', client.connack_string(rc))
+        mqttc.subscribe('/'.join([self.shadow, 'update', 'delta']), qos=1)
+        mqttc.subscribe('/'.join([self.shadow, 'update', 'rejected']), qos=1)
+
+    def on_disconnect(self, mqttc, userdata, rc):
+        """MQTT on disconnect callback function implementation."""
+        if rc != client.MQTT_ERR_SUCCESS:
+            LOG.error('on_disconnect: Unexpected disconnection.')
+
+    def on_subscribe(self, mqttc, userdata, mid, granted_qos):
+        """MQTT on subscribe callback function implementation."""
+        LOG.debug('on_subscribe: mid: %s QoS: %s', mid, granted_qos)
+
+    def on_message(self, mqttc, userdata, message):
+        """MQTT on message callback function implementation."""
+        LOG.debug('on_message: topic: %s payload: %s', message.topic,
+                  message.payload)
+        if message.topic == '/'.join([self.shadow, 'update', 'delta']):
+            payload = json.loads(message.payload)
+            LOG.debug('JSONRPC: %s', json.dumps(payload['state']))
+            response = xbmc.executeJSONRPC(json.dumps(payload['state']))
+            payload['state']['response'] = json.loads(response)
+            payload['state']['response'].pop('jsonrpc')
+            payload['state']['response'].pop('id')
+            state = {'state': {'reported': payload['state']['response'],
+                               'desired':None}}
+            LOG.debug('publishing state %s', state)
+            info = mqttc.publish('/'.join([self.shadow, 'update']),
+                                 json.dumps(state), qos=1)
+            if info.rc != client.MQTT_ERR_SUCCESS:
+                LOG.error('problem publishing to shadow %s.', str(info))
+        elif message.topic == '/'.join([self.shadow, 'update', 'rejected']):
+            LOG.error('rejected: %s', message.payload)
 
     @property
     def version(self):
@@ -58,6 +119,16 @@ class Kodiot(xbmc.Monitor):
     def port(self):
         """Return the port setting."""
         return int(self.addon.getSetting('port').strip())
+
+    @property
+    def thing(self):
+        """Return the Thing name setting."""
+        return self.addon.getSetting('thing').strip()
+
+    @property
+    def shadow(self):
+        """Return the shadow path root."""
+        return '$aws/things/%s/shadow' % self.thing
 
     @property
     def root_ca(self):
@@ -79,64 +150,17 @@ class Kodiot(xbmc.Monitor):
         """Return the keepalive setting."""
         return float(self.addon.getSetting('keepalive').strip())
 
-    def on_connect(self, mqttc, userdata, flags, rc):
-        """MQTT on connect callback function implementation."""
-        LOG.info('on_connect: %s', client.connack_string(rc))
-        mqttc.subscribe('$aws/things/kodi/shadow/update/delta', qos=1)
-        mqttc.subscribe('$aws/things/kodi/shadow/update/rejected', qos=1)
-
-    def on_disconnect(self, mqttc, userdata, rc):
-        """MQTT on disconnect callback function implementation."""
-        if rc != client.MQTT_ERR_SUCCESS:
-            LOG.error('on_disconnect: Unexpected disconnection.')
-
-    def on_subscribe(self, mqttc, userdata, mid, granted_qos):
-        """MQTT on subscribe callback function implementation."""
-        LOG.debug('on_subscribe: mid: %s QoS: %s', mid, granted_qos)
-
-    def on_message(self, mqttc, userdata, message):
-        """MQTT on message callback function implementation."""
-        LOG.debug('on_message: topic: %s payload: %s', message.topic,
-                  message.payload)
-        if message.topic == '$aws/things/kodi/shadow/update/delta':
-            payload = json.loads(message.payload)
-            LOG.debug('JSONRPC: %s', json.dumps(payload['state']))
-            response = xbmc.executeJSONRPC(json.dumps(payload['state']))
-            payload['state']['response'] = json.loads(response)
-            payload['state']['response'].pop('jsonrpc')
-            payload['state']['response'].pop('id')
-            state = json.dumps({'state': {'reported': payload['state']['response'],
-                                          'desired':None}})
-            LOG.debug('publishing state %s', state)
-            info = mqttc.publish('$aws/things/kodi/shadow/update', state, qos=1)
-            if info.rc != client.MQTT_ERR_SUCCESS:
-                LOG.error('problem publishing to shadow %s.', str(info))
-        elif message.topic == '$aws/things/kodi/shadow/update/rejected':
-            LOG.error('rejected: %s', message.payload)
-
 
 def main():
     """Main Program."""
     kodiot = Kodiot()
-    LOG.info('Launching Kodiot %s, MQTT version %s', kodiot.version,
-             mqtt.__version__)
-
-    mqttc = client.Client()
-    mqttc.on_connect = kodiot.on_connect
-    mqttc.on_disconnect = kodiot.on_disconnect
-    mqttc.on_subscribe = kodiot.on_subscribe
-    mqttc.on_message = kodiot.on_message
-    mqttc.tls_set(kodiot.root_ca, certfile=kodiot.pem, keyfile=kodiot.key,
-                  cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2)
-
-    # Connect with MQTT Broker
-    mqttc.connect(kodiot.host, kodiot.port, kodiot.keepalive)
-    mqttc.loop_start()
+    kodiot.start()
 
     while not kodiot.waitForAbort(1.0):
         pass
 
-    mqttc.loop_stop()
+    kodiot.stop()
+
 
 if __name__ == '__main__':
     main()
